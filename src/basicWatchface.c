467d99e9d07062568567bef59449401917167ad7
JeremyCarlsten <jeremy@jeremycarlsten.com>
2016-01-25 14:07:22 -0600

    Adding a new font and playing with colors

--- src/basicWatchface.c
+++ src/basicWatchface.c
@@ -1,8 +1,9 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+
 
 static void update_time(){
   time_t temp = time(NULL);
@@ -18,12 +19,13 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_30));
   s_time_layer = text_layer_create(GRect(0,PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
 
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
@@ -36,6 +38,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  fonts_unload_custom_font(s_time_font);
 }
 
 static void init() {
