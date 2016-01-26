d37c87df52115770c740be20f021e60dac6e5b06
JeremyCarlsten <jeremy@jeremycarlsten.com>
2016-01-26 14:33:25 -0600

    adding a fancier watchface for future fun

--- src/basicWatchface.c
+++ src/basicWatchface.c
@@ -1,9 +1,14 @@
 #include <pebble.h>
 
-static Window *s_main_window;
-static TextLayer *s_time_layer;
-static GFont s_time_font;
-
+// BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
+static Window *s_window;
+static GFont s_res_bitham_30_black;
+static GFont s_res_roboto_condensed_21;
+static TextLayer *s_time_text_layer;
+static TextLayer *s_temperature_text_layer;
+static BitmapLayer *s_bitmaplayer_1;
+static TextLayer *s_textlayer_1;
+static TextLayer *s_textlayer_2;
 
 static void update_time(){
   time_t temp = time(NULL);
@@ -12,55 +17,84 @@ static void update_time(){
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
 
-  text_layer_set_text(s_time_layer, s_buffer);
+  text_layer_set_text(s_time_text_layer, s_buffer);
 }
 
-static void main_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+static void initialise_ui(void) {
+  s_window = window_create();
+  window_set_background_color(s_window, GColorBlack);
+  #ifndef PBL_SDK_3
+    window_set_fullscreen(s_window, 1);
+  #endif
 
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_30));
-  s_time_layer = text_layer_create(GRect(0,PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
+  s_res_bitham_30_black = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK);
+  s_res_roboto_condensed_21 = fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21);
+  // s_time_text_layer
+  s_time_text_layer = text_layer_create(GRect(0, 16, 144, 36));
+  text_layer_set_text(s_time_text_layer, "03:04");
+  text_layer_set_text_alignment(s_time_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_time_text_layer, s_res_bitham_30_black);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_time_text_layer);
 
-  
-  window_layer_set_background_color(window_layer, GColorBlack);
-  text_layer_set_background_color(s_time_layer, GColorRed);
-  text_layer_set_text_color(s_time_layer, GColorWhite);
-  text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, s_time_font);
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  // s_temperature_text_layer
+  s_temperature_text_layer = text_layer_create(GRect(2, 141, 34, 49));
+  text_layer_set_background_color(s_temperature_text_layer, GColorBlack);
+  text_layer_set_text_color(s_temperature_text_layer, GColorWhite);
+  text_layer_set_text(s_temperature_text_layer, "0");
+  text_layer_set_font(s_temperature_text_layer, s_res_roboto_condensed_21);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_temperature_text_layer);
 
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  // s_bitmaplayer_1
+  s_bitmaplayer_1 = bitmap_layer_create(GRect(3, 110, 33, 31));
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_bitmaplayer_1);
 
+  // s_textlayer_1
+  s_textlayer_1 = text_layer_create(GRect(15, 52, 116, 25));
+  text_layer_set_text(s_textlayer_1, "01/24/2016");
+  text_layer_set_text_alignment(s_textlayer_1, GTextAlignmentCenter);
+  text_layer_set_font(s_textlayer_1, s_res_roboto_condensed_21);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_1);
+
+  // s_textlayer_2
+  s_textlayer_2 = text_layer_create(GRect(102, 143, 38, 21));
+  text_layer_set_background_color(s_textlayer_2, GColorBlack);
+  text_layer_set_text_color(s_textlayer_2, GColorWhite);
+  text_layer_set_text(s_textlayer_2, "N");
+  text_layer_set_font(s_textlayer_2, s_res_roboto_condensed_21);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_2);
 }
+
+static void destroy_ui(void) {
+  window_destroy(s_window);
+  text_layer_destroy(s_time_text_layer);
+  text_layer_destroy(s_temperature_text_layer);
+  bitmap_layer_destroy(s_bitmaplayer_1);
+  text_layer_destroy(s_textlayer_1);
+  text_layer_destroy(s_textlayer_2);
+}
+// END AUTO-GENERATED UI CODE
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
 
-static void main_window_unload(Window *window) {
-  text_layer_destroy(s_time_layer);
-  fonts_unload_custom_font(s_time_font);
+static void handle_window_unload(Window* window) {
+  destroy_ui();
 }
 
 static void init() {
-  // Create main Window element and assign to pointer
-  s_main_window = window_create();
-
-  // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
+  initialise_ui();
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .unload = handle_window_unload,
   });
+  window_stack_push(s_window, true);
 
-  // Show the Window on the watch, with animated=true
-  window_stack_push(s_main_window, true);
   update_time();
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
-static void deinit(){
-  window_destroy(s_main_window);
+static void deinit() {
+  window_stack_remove(s_window, true);
 }
 
 int main(void) {
