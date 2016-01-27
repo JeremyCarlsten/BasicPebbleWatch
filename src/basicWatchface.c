a6331c8781a5bbdf82321ce0862a8bec15abf086
JeremyCarlsten <jeremy@jeremycarlsten.com>
2016-01-27 11:11:23 -0600

    Adding weather data

--- src/basicWatchface.c
+++ src/basicWatchface.c
@@ -1,4 +1,7 @@
 #include <pebble.h>
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+
 
 // BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
@@ -8,7 +11,9 @@ static TextLayer *s_time_text_layer;
 static TextLayer *s_temperature_text_layer;
 static BitmapLayer *s_bitmaplayer_1;
 static TextLayer *s_date_text_layer;
+// static char degreeSymbol[6] = "\u00B0";
 static TextLayer *s_direction_text_layer;
+
 
 static void update_time(){
   time_t temp = time(NULL);
@@ -41,10 +46,10 @@ static void initialise_ui(void) {
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_time_text_layer);
 
   // s_temperature_text_layer
-  s_temperature_text_layer = text_layer_create(GRect(2, 141, 34, 49));
+  s_temperature_text_layer = text_layer_create(GRect(2, 141, 150, 49));
   text_layer_set_background_color(s_temperature_text_layer, GColorBlack);
   text_layer_set_text_color(s_temperature_text_layer, GColorWhite);
-  text_layer_set_text(s_temperature_text_layer, "0");
+  text_layer_set_text(s_temperature_text_layer, "...");
   text_layer_set_font(s_temperature_text_layer, s_res_roboto_condensed_21);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_temperature_text_layer);
 
@@ -81,11 +86,58 @@ static void destroy_ui(void) {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+
+  // Get weather update every 30 minutes
+ if(tick_time->tm_min % 30 == 0) {
+   // Begin dictionary
+   DictionaryIterator *iter;
+   app_message_outbox_begin(&iter);
+
+   // Add a key-value pair
+   dict_write_uint8(iter, 0, 0);
+
+   // Send the message!
+   app_message_outbox_send();
+ }
 }
 
 static void handle_window_unload(Window* window) {
   destroy_ui();
 }
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+
+  // Read tuples for data
+  Tuple *temp_tuple = dict_find(iterator, KEY_TEMPERATURE);
+  Tuple *conditions_tuple = dict_find(iterator, KEY_CONDITIONS);
+
+  // If all data is available, use it
+  if(temp_tuple && conditions_tuple) {
+    snprintf(temperature_buffer, sizeof(temperature_buffer), "%dF", (int)temp_tuple->value->int32);
+    snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+
+    // Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_temperature_text_layer, weather_layer_buffer);
+  }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 
 static void init() {
   initialise_ui();
@@ -94,9 +146,20 @@ static void init() {
   });
   window_stack_push(s_window, true);
 
+  //start date/time
   update_time();
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   tick_timer_service_subscribe(DAY_UNIT, tick_handler);
+
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
 }
 
 static void deinit() {
